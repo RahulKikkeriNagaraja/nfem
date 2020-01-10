4317817896e202435e3461dc0079002ea9b503e0
Rahul Kikkeri Nagaraja <ge73xip@mytum.de>
2020-01-10 23:17:16 +0100

    Colabissue + added pyqt5

---
+++ ColabIssue.md
@@ -0,0 +1,6 @@
+
+* !pip install git+<remote repository> doesn't work from GitLab.
+
+* *%matplotlib notebook* used for interactive plots does not work in Colab
+
+* If any changes are made, restart runtime and Run the previous cells again
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
    description='',
    author='ge73xip',
    author_email='ge73xip@mytum.de',
-   url='https://github.com/ge73xip/nfem/tree/pipBranch/',
+   url='https://github.com/ge73xip/nfem.git',
    packages=['nfem', 'nfem.visualization', 'nfem.tests'],
    install_requires=['numpy', 'matplotlib', 'pyqt5'],
 )
